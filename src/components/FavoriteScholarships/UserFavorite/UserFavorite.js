be4757c0dc810f01481891de4bf6c103274744f0
Victor Levy <victorlevygp@gmail.com>
2019-09-03 01:59:53 -0300

    desktop done. nedd to determine a grey color, stylize dropdown, cities(suggestion or dropdown), navbar mobile not working, no microinteractions. no localstorage. no id for courses

--- src/components/FavoriteScholarships/UserFavorite/UserFavorite.js
+++ src/components/FavoriteScholarships/UserFavorite/UserFavorite.js
@@ -1,24 +1,118 @@
 import React from "react";
 
+const drawStars = fav => {
+  let stars = [];
+  for (let i = 1; i <= 5; i += 1) {
+    if (i <= fav.university.score) {
+      stars.push(
+        <svg
+          key={i}
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="star"
+          className="svg-inline--fa fa-star fa-w-18 filled-star"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+        >
+          <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+        </svg>
+      );
+    } else if (i === Math.ceil(fav.university.score)) {
+      stars.push(
+        <svg
+          key={i}
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="star-half-alt"
+          className="svg-inline--fa fa-star-half-alt fa-w-17 half-star"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 536 512"
+        >
+          <path d="M508.55 171.51L362.18 150.2 296.77 17.81C290.89 5.98 279.42 0 267.95 0c-11.4 0-22.79 5.9-28.69 17.81l-65.43 132.38-146.38 21.29c-26.25 3.8-36.77 36.09-17.74 54.59l105.89 103-25.06 145.48C86.98 495.33 103.57 512 122.15 512c4.93 0 10-1.17 14.87-3.75l130.95-68.68 130.94 68.7c4.86 2.55 9.92 3.71 14.83 3.71 18.6 0 35.22-16.61 31.66-37.4l-25.03-145.49 105.91-102.98c19.04-18.5 8.52-50.8-17.73-54.6zm-121.74 123.2l-18.12 17.62 4.28 24.88 19.52 113.45-102.13-53.59-22.38-11.74.03-317.19 51.03 103.29 11.18 22.63 25.01 3.64 114.23 16.63-82.65 80.38z"></path>
+        </svg>
+      );
+    } else {
+      stars.push(
+        <svg
+          key={i}
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="star"
+          className="svg-inline--fa fa-star fa-w-18 empty-star"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+        >
+          <path d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
+        </svg>
+      );
+    }
+  }
+  return stars;
+};
+
 const UserFavorite = ({ favorite, deleteFavorite }) => {
   return (
     <div className="favorite-card">
-      <img src={favorite.university.logo_url} alt={favorite.university.name} />
+      <div className="favorite-university-logo-container">
+        <img
+          src={favorite.university.logo_url}
+          alt={favorite.university.name}
+        />
+      </div>
       <h5 className="upper-bold">{favorite.university.name}</h5>
       <h6>{favorite.course.name}</h6>
-      <span className="rating-text">{favorite.university.score}</span>
-      {/* <i>ESTRELAS</i> */}
+      <div className="rating-container">
+        <span className="rating-text">
+          {favorite.university.score.toFixed(1)}
+        </span>
+        <div className="stars">{drawStars(favorite)}</div>
+      </div>
       <div className="favorite-card-course-info-center">
         <p className="upper-bold">
-          {favorite.course.kind} - {favorite.course.shift}
+          {favorite.course.kind === "EaD"
+            ? "Ensino a distância"
+            : favorite.course.kind === "Presencial"
+            ? "Presencial"
+            : null}{" "}
+          - {favorite.course.shift}
         </p>
-        <p className="favorite-start-date">Início das aulas em: {favorite.start_date}</p>
+        <p className="favorite-start-date">
+          Início das aulas em: {favorite.start_date}
+        </p>
       </div>
-      <p className="favorite-price-title">Mensalidade com o Quero Bolsa:</p>
-      <p className="favorite-full-price">R${favorite.full_price.toLocaleString("pt-BR", {
-    minimumFractionDigits: 2
-  })}</p>
-      <p className="favorite-discounted-price"><span className="green-bold">R$ {favorite.price_with_discount}</span>/mês</p>
+      {favorite.enabled ? (
+        <div className="favorite-card-bottom-info">
+          <p className="favorite-price-title">Mensalidade com o Quero Bolsa:</p>
+          <p className="favorite-full-price">
+            R$
+            {favorite.full_price.toLocaleString("pt-BR", {
+              minimumFractionDigits: 2
+            })}
+          </p>
+          <p className="favorite-discounted-price">
+            <span className="green-bold">
+              R${" "}
+              {favorite.price_with_discount.toLocaleString("pt-BR", {
+                minimumFractionDigits: 2
+              })}
+            </span>
+            /mês
+          </p>
+        </div>
+      ) : (
+        <div className="favorite-card-bottom-info">
+          <p className="favorite-price-title">Bolsa indisponível</p>
+          <p class="favorite-unavailable-text">
+            Entre em contato com nosso atendimento para saber mais
+          </p>
+        </div>
+      )}
       <div className="favorite-card-buttons">
         <button
           value={JSON.stringify(favorite)}
@@ -30,7 +124,9 @@ const UserFavorite = ({ favorite, deleteFavorite }) => {
         {favorite.enabled ? (
           <button className="modal-add-btn">Ver oferta</button>
         ) : (
-          <button className="modal-add-btn" disabled>Indisponível</button>
+          <button className="modal-add-btn" disabled>
+            Indisponível
+          </button>
         )}
       </div>
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,10 +19,10 @@ const Footer = () => {
           </svg>
           <div className="footer-top-larger-text">
             <span className="footer-top-larger-text-top">0800 123 222</span>
-            <span className="footer-top-larger-text-bottom mobile-content">
+            <span className="footer-top-text-bottom mobile-content">
               Segunda a sexta de 8h às 22h
             </span>
-            <span className="footer-top-larger-text-bottom desktop-content">
+            <span className="footer-top-text-bottom desktop-content">
               Seg - Sex 8h-22h
             </span>
           </div>
@@ -39,16 +39,15 @@ const Footer = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 576 512"
             >
-              <path
-                d="M532 386.2c27.5-27.1 44-61.1 44-98.2 0-80-76.5-146.1-176.2-157.9C368.3 72.5 294.3 32 208 32 93.1 32 0 103.6 0 192c0 37 16.5 71 44 98.2-15.3 30.7-37.3 54.5-37.7 54.9-6.3 6.7-8.1 16.5-4.4 25 3.6 8.5 12 14 21.2 14 53.5 0 96.7-20.2 125.2-38.8 9.2 2.1 18.7 3.7 28.4 4.9C208.1 407.6 281.8 448 368 448c20.8 0 40.8-2.4 59.8-6.8C456.3 459.7 499.4 480 553 480c9.2 0 17.5-5.5 21.2-14 3.6-8.5 1.9-18.3-4.4-25-.4-.3-22.5-24.1-37.8-54.8zm-392.8-92.3L122.1 305c-14.1 9.1-28.5 16.3-43.1 21.4 2.7-4.7 5.4-9.7 8-14.8l15.5-31.1L77.7 256C64.2 242.6 48 220.7 48 192c0-60.7 73.3-112 160-112s160 51.3 160 112-73.3 112-160 112c-16.5 0-33-1.9-49-5.6l-19.8-4.5zM498.3 352l-24.7 24.4 15.5 31.1c2.6 5.1 5.3 10.1 8 14.8-14.6-5.1-29-12.3-43.1-21.4l-17.1-11.1-19.9 4.6c-16 3.7-32.5 5.6-49 5.6-54 0-102.2-20.1-131.3-49.7C338 339.5 416 272.9 416 192c0-3.4-.4-6.7-.7-10C479.7 196.5 528 238.8 528 288c0 28.7-16.2 50.6-29.7 64z"
-              ></path>
+              <path d="M532 386.2c27.5-27.1 44-61.1 44-98.2 0-80-76.5-146.1-176.2-157.9C368.3 72.5 294.3 32 208 32 93.1 32 0 103.6 0 192c0 37 16.5 71 44 98.2-15.3 30.7-37.3 54.5-37.7 54.9-6.3 6.7-8.1 16.5-4.4 25 3.6 8.5 12 14 21.2 14 53.5 0 96.7-20.2 125.2-38.8 9.2 2.1 18.7 3.7 28.4 4.9C208.1 407.6 281.8 448 368 448c20.8 0 40.8-2.4 59.8-6.8C456.3 459.7 499.4 480 553 480c9.2 0 17.5-5.5 21.2-14 3.6-8.5 1.9-18.3-4.4-25-.4-.3-22.5-24.1-37.8-54.8zm-392.8-92.3L122.1 305c-14.1 9.1-28.5 16.3-43.1 21.4 2.7-4.7 5.4-9.7 8-14.8l15.5-31.1L77.7 256C64.2 242.6 48 220.7 48 192c0-60.7 73.3-112 160-112s160 51.3 160 112-73.3 112-160 112c-16.5 0-33-1.9-49-5.6l-19.8-4.5zM498.3 352l-24.7 24.4 15.5 31.1c2.6 5.1 5.3 10.1 8 14.8-14.6-5.1-29-12.3-43.1-21.4l-17.1-11.1-19.9 4.6c-16 3.7-32.5 5.6-49 5.6-54 0-102.2-20.1-131.3-49.7C338 339.5 416 272.9 416 192c0-3.4-.4-6.7-.7-10C479.7 196.5 528 238.8 528 288c0 28.7-16.2 50.6-29.7 64z"></path>
             </svg>
-            <span className="mobile-content">Chat</span>
-            <span className="desktop-content">Chato ao vivo</span>
-            <span className="desktop-content">Seg - Sex 8h-22h</span>
+            <div className="footer-top-small-item-text">
+              <span className="mobile-content">Chat</span>
+              <span className="footer-top-text-top desktop-content">Chato ao vivo</span>
+              <span className="footer-top-text-bottom desktop-content">Seg - Sex 8h-22h</span>
+            </div>
           </div>
           <div className="footer-top-small-item">
-            {/* <FontAwesomeIcon icon={faEnvelope} /> */}
             <svg
               aria-hidden="true"
               focusable="false"
@@ -61,9 +60,11 @@ const Footer = () => {
             >
               <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
             </svg>
-            <span className="mobile-content">E-mail</span>
-            <span className="desktop-content">Mande um e-mail</span>
-            <span className="desktop-content">Respondemos rapidinho</span>
+            <div className="footer-top-small-item-text">
+              <span className="mobile-content">E-mail</span>
+              <span className="footer-top-text-top desktop-content">Mande um e-mail</span>
+              <span className="footer-top-text-bottom desktop-content">Respondemos rapidinho</span>
+            </div>
           </div>
           <div className="footer-top-small-item">
             <svg
@@ -78,9 +79,13 @@ const Footer = () => {
             >
               <path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path>
             </svg>
-            <span className="mobile-content">Ajuda</span>
-            <span className="desktop-content">Central de ajuda</span>
-            <span className="desktop-content">Encontre todas as respostas</span>
+            <div className="footer-top-small-item-text">
+              <span className="mobile-content">Ajuda</span>
+              <span className="footer-top-text-top desktop-content">Central de ajuda</span>
+              <span className="footer-top-text-bottom desktop-content">
+                Encontre todas as respostas
+              </span>
+            </div>
           </div>
         </div>
       </div>
